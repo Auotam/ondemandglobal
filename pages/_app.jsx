5a077f00b9c025b8e2cea8f180dd31e35f6957c1
Shivam Sharma <shivam.sharma@acefone.com>
2024-05-04 18:41:38 +0530

    font-update

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -12,9 +12,7 @@ import Preloader from '../components/preloader';
 import SEO from '../components/seo';
 import useUserData from '@/utils/UseUserdata';
 import "../styles/globals.css"
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flat-ui/2.3.0/css/flat-ui.min.css" 
-integrity="sha512-6f7HT84a/AplPkpSRSKWqbseRTG4aRrhadjZezYQ0oVk/B+nm/US5KzQkyyOyh0Mn9cyDdChRdS9qaxJRHayww==" 
-crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 
 let persistor = persistStore(store)
 
@@ -23,7 +21,7 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-    <SEO font={'https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&family=Roboto:wght@400;500;700&display=swap'} />
+    <SEO font={'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet'} />
       <Provider store={store}>
         <PersistGate loading={<Preloader/>} persistor={persistor}>
         <AppProvider>
